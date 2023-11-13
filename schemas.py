28f787825cee3b957f1dc8399dcfacaff8702b18
Vanessa Dickey <vcdickey@gmail.com>
2023-11-13 16:30:54 +0000

    caught up typing with tutorial

--- schemas.py
+++ schemas.py
@@ -2,19 +2,31 @@ from typing import Optional
 
 from pydantic import BaseModel, EmailStr
 
-class CategorySchema(BaseModel):
-    id: int
-    name: str
-
-class CuisineSchema(BaseModel):
-    id: int
-    name: str
-
-class MenuItemSchema(BaseModel):
-    id: int
+class ProductSchema(BaseModel):
     title: str
-    description: str
-    price: float
-    spicy_level: int
-    category: CategorySchema
-    cuisine: CuisineSchema
+    description: str | None = None
+    at_sale: bool = False
+    inventory: int
+
+# class UserCreate(UserBase):
+
+# from typing import Optional
+
+# from pydantic import BaseModel, EmailStr
+
+# class CategorySchema(BaseModel):
+#     id: int
+#     name: str
+
+# class CuisineSchema(BaseModel):
+#     id: int
+#     name: str
+
+# class MenuItemSchema(BaseModel):
+#     id: int
+#     title: str
+#     description: str
+#     price: float
+#     spicy_level: int
+#     category: CategorySchema
+#     cuisine: CuisineSchema
