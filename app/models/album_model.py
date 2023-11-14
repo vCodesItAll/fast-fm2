556690be7f614581acadec032143d09b83b60d7e
Vanessa Dickey <vcdickey@gmail.com>
2023-11-14 11:36:21 +0000

    made album model

--- app/models/album_model.py
+++ app/models/album_model.py
@@ -1,11 +1,14 @@
-import sys
-from pathlib import Path
+from app.utils.imports import app, router, Base, engine, get_db, ProductSchema, StarletteHTTPException, PlainTextResponse, CORSMiddleware, sys, Path, relationship, Column, Integer, String, Date, ForeignKey, Table
 
-# Add the path to the utils module to sys.path
-sys.path.append(str(Path(__file__).resolve().parent.parent / 'utils'))
 
-from database import Base
-from sqlalchemy import Column, Integer, String, Date
+
+# Junction table for many-to-many relationship between albums and artists
+album_artists = Table(
+    'album_artists',
+    Base.metadata,
+    Column('album_id', Integer, ForeignKey('albums.id')),
+    Column('artist_id', Integer, ForeignKey('artists.id'))
+)
 
 class Album(Base):
     __tablename__ = "albums"
@@ -13,3 +16,11 @@ class Album(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     release_date = Column(Date)
+    # Other album attributes here
+
+    # Define many-to-many relationship between albums and artists
+    artists = relationship("Artist", secondary=album_artists, back_populates="albums")
+
+    # Stretch goal - returning album data as a dictionary
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
--- app/utils/imports.py
+++ app/utils/imports.py
@@ -5,9 +5,9 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 
 # Import SQLAlchemy modules for database operations
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, Column, Integer, String, Date, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, relationship
 
 # Import Pydantic modules for data validation and serialization
 from pydantic import BaseModel
@@ -23,4 +23,11 @@ app = FastAPI()
 router = APIRouter()
 Base = declarative_base()
 
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent / 'utils'))
 
+
+# copy this import into other files
+# from app.utils.imports import app, router, Base, engine, get_db, ProductSchema, StarletteHTTPException, PlainTextResponse, CORSMiddleware, sys, Path, relationship
+from app.utils.imports import app, router, Base, engine, get_db, ProductSchema, StarletteHTTPException, PlainTextResponse, CORSMiddleware, sys, Path, relationship, Column, Integer, String, Date, ForeignKey, Table
