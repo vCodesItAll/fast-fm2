f2ff5cd4ce7230ac44ec365ee9d9c65b27dcae6b
Vanessa Dickey <vcdickey@gmail.com>
2023-11-15 18:21:06 +0000

    made user schema

---
+++ app/schemas/user_schema.py
@@ -0,0 +1,14 @@
+from app.utils.imports import app, router, Base, engine, get_db, ProductSchema, StarletteHTTPException, PlainTextResponse, CORSMiddleware, sys, Path, relationship, Column, Integer, String, Date, ForeignKey, Table, relationship, DateTime, BaseModel, Optional, List
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
