076132cebf968be1390a418088a451c3a2cd267e
Vanessa Dickey <vcdickey@gmail.com>
2023-11-17 14:52:49 +0000

    truncated main.py imports to one line

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -1,24 +1,24 @@
-from fastapi.testclient import TestClient
+# from fastapi.testclient import TestClient
 
 
-# importing the sys module
-import sys        
+# # importing the sys module
+# import sys        
  
-# inserting the mod.py directory at 
-# position 1 in sys.path
-sys.path.insert(1, '/workspace/fastapi-docker/') 
+# # inserting the mod.py directory at 
+# # position 1 in sys.path
+# sys.path.insert(1, '/workspace/fastapi-docker/') 
 
-from main import app
+# from main import app
 
-client = TestClient(app)
+# client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"msg": "Hello World"}
 
-def test_read_item():
-    tres = 3
-    response = client.get("/items/" + str(tres))
-    assert response.status_code == 200
-    assert response.json() == {"item_id": tres}
+# def test_read_item():
+#     tres = 3
+#     response = client.get("/items/" + str(tres))
+#     assert response.status_code == 200
+#     assert response.json() == {"item_id": tres}
