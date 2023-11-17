198982915c5f08fb2dccd4183b4a9cff4d2029ea
Vanessa Dickey <vcdickey@gmail.com>
2023-11-17 00:34:53 +0000

    added an init py file to schemas folder

--- schemas.py
+++ schemas.py
@@ -1,32 +1,32 @@
-from typing import Optional
-from pydantic import BaseModel, EmailStr
-
-class ProductSchema(BaseModel):
-    title: str
-    description: str | None = None
-    at_sale: bool = False
-    inventory: int
-    added_at: str
-
-# class UserCreate(UserBase):
-
 # from typing import Optional
-
 # from pydantic import BaseModel, EmailStr
 
-# class CategorySchema(BaseModel):
-#     id: int
-#     name: str
-
-# class CuisineSchema(BaseModel):
-#     id: int
-#     name: str
-
-# class MenuItemSchema(BaseModel):
-#     id: int
+# class ProductSchema(BaseModel):
 #     title: str
-#     description: str
-#     price: float
-#     spicy_level: int
-#     category: CategorySchema
-#     cuisine: CuisineSchema
+#     description: str | None = None
+#     at_sale: bool = False
+#     inventory: int
+#     added_at: str
+
+# # class UserCreate(UserBase):
+
+# # from typing import Optional
+
+# # from pydantic import BaseModel, EmailStr
+
+# # class CategorySchema(BaseModel):
+# #     id: int
+# #     name: str
+
+# # class CuisineSchema(BaseModel):
+# #     id: int
+# #     name: str
+
+# # class MenuItemSchema(BaseModel):
+# #     id: int
+# #     title: str
+# #     description: str
+# #     price: float
+# #     spicy_level: int
+# #     category: CategorySchema
+# #     cuisine: CuisineSchema
