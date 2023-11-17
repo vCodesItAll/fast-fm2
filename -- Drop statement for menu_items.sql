dab1fa89c5a6d174a13d039ce27062e8d5c6b5f1
Vanessa Dickey <vcdickey@gmail.com>
2023-11-17 00:23:48 +0000

    made create table queries in db_database_map.sql

--- -- Drop statement for menu_items.sql
+++
@@ -1,8 +0,0 @@
--- Drop statement for menu_items
-DROP TABLE IF EXISTS menu_items;
-
--- Drop statement for categories
-DROP TABLE IF EXISTS categories;
-
--- Drop statement for cuisines
-DROP TABLE IF EXISTS cuisines;
